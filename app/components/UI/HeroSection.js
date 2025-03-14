c772b1bf3e4e25b9343db7e7f2322b3fc45f185b
Nahun Tadeo Quiroz Garcia <nahun.tadeo.quiroz.garcia20@gmail.com>
2025-03-14 11:46:30 -0600

    Update HeroSection  NTQG04

--- app/components/UI/HeroSection.js
+++ app/components/UI/HeroSection.js
@@ -7,7 +7,7 @@ const HeroSection = () => {
   return (
     <div className="relative w-full h-[400px] md:h-[500px] flex items-center justify-center">
       
-      {/* Imagen de fondo */}
+      
       <div className="absolute top-0 left-0 w-full h-full">
         <Image 
           src="/img/imagenFondo/mich.webp" 
@@ -23,7 +23,7 @@ const HeroSection = () => {
           {/* Sección superior con título y ubicación */}
           <div className="flex flex-wrap justify-between items-center w-full gap-4">
             
-            {/* Texto principal */}
+            
             <div className="text-center md:text-left">
               <h2 className="text-xl md:text-2xl font-bold text-[var(--color-primary)]">
                 Descubre Michoacán
@@ -31,14 +31,14 @@ const HeroSection = () => {
               <p className="text-gray-600">El Alma de México</p>
             </div>
 
-            {/* Ubicación y hora */}
+            
             <div className="bg-white rounded-full shadow-md px-4 py-2 flex items-center border border-gray-300 text-sm font-medium whitespace-nowrap">
               <FaMapMarkerAlt className="text-[#EA5261] text-lg mr-2" />
               <p className="text-gray-700">En Maravatío | 10:30 h</p>
             </div>
           </div>
 
-          {/* Barra de búsqueda - Responsiva */}
+          
           <div className="flex w-full mt-4 items-center border border-gray-300 rounded-full overflow-hidden shadow-sm">
             <input
               type="text"
