86e2eb501199edf661391009d8e8bd97a39ce1e1
JessR28 <jessica.soto.rivera1401@gmail.com>
2025-03-21 15:47:35 -0600

    Create Galeria JSR03

---
+++ app/components/UI/Galeria.js
@@ -0,0 +1,48 @@
+'use client'
+
+const galeria = [
+  { src: "/img/imagesGoMich/Fondo.jpg", titulo: "Uruapan" },
+  { src: "/img/imagesGoMich/Fondo.webp", titulo: "Pátzcuaro" },
+  { src: "/img/imagesGoMich/mich.webp", titulo: "Morelia" },
+  { src: "/img/imagesGoMich/Fondo.jpg", titulo: "Zirahuén" },
+  { src: "/img/imagesGoMich/Fondo.webp", titulo: "Paracho" },
+  { src: "/img/imagesGoMich/mich.webp", titulo: "Tzintzuntzan" },
+  { src: "/img/imagesGoMich/Fondo.jpg", titulo: "Tacámbaro" },
+  { src: "/img/imagesGoMich/Fondo.webp", titulo: "Santa Clara del Cobre" },
+  { src: "/img/imagesGoMich/mich.webp", titulo: "Angangueo" },
+  { src: "/img/imagesGoMich/Fondo.jpg", titulo: "Tlalpujahua" }
+]
+
+const Galeria = () => {
+  return (
+    <section className="py-12 bg-gray-100">
+      <div className="max-w-[1200px] mx-auto px-6">
+        <h2 className="text-3xl font-bold text-gray-800 mb-2 md:mb-4">Rostros y Paisajes de Michoacán</h2>
+        <div className="w-50 h-[5px] bg-accent mb-6"></div>
+
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4">
+          {galeria.map((item, index) => (
+            <div 
+              key={index} 
+              className="relative overflow-hidden rounded-lg group transition-all duration-500 shadow-sm hover:scale-105"
+            >
+              {/* Imagen */}
+              <img 
+                src={item.src} 
+                alt={item.titulo} 
+                className="w-full h-[140px] sm:h-[180px] lg:h-[200px] object-cover transition-transform duration-500 group-hover:scale-110"
+              />
+
+              {/* Capa con efecto de hover (color accent) */}
+              <div className="absolute inset-0 flex items-center justify-center text-white text-lg font-semibold bg-accent opacity-0 group-hover:opacity-80 transition-all duration-500">
+                {item.titulo}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Galeria
--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import TopDestinos from "./components/UI/TopDestinos.js"
 import Eventos from "./components/UI/Eventos"
 import PueblosMagicos from "./components/UI/PueblosMagicos"
 import NoticiasRecientes from "./components/UI/NoticiasRecientes"
+import Galeria from "./components/UI/Galeria"
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <Eventos />
       <PueblosMagicos />
       <NoticiasRecientes />
+      <Galeria />
     </>
   )
 }
